8ebd6a89a34696583c2bbbbf4b2c45b8d6acd93f
zephinzer <dev@joeir.net>
2018-01-15 22:01:28 +0800

    added `supertest` for testing server and modified reporters for mocha and mocha-watch

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,6 @@ printf "using mocha              : ${MOCHA_VERSION}\n";
 printf "test run started on      : $(date +'%d-%m-%Y %H:%M:%S')\n";
 
 istanbul cover ./node_modules/.bin/_mocha -- \
+  --reporter dot \
   --forbid-only \
   --forbid-pending "./test/entrypoint.js" "./test/**/*.test.js";
