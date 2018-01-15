36a52ffe1420ac7b3704645b55afc46069d353d3
zephinzer <dev@joeir.net>
2018-01-15 20:53:03 +0800

    added cpu load stats to metrics

--- middleware/prometheus.js
+++ middleware/prometheus.js
@@ -1,7 +1,53 @@
 const expressPromBundle = require('express-prom-bundle');
+const Prometheus = require('prom-client');
+const osUtils = require('os-utils');
+
+const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
+
+const cpuUsage = new Prometheus.Gauge({
+  name: 'node_cpu_usage',
+  help: 'CPU usage (%)',
+});
+(function cpuUsageReporting() {
+  osUtils.cpuUsage((value) => {
+    cpuUsage.set(value);
+    setTimeout(cpuLoadReporting, ONE_SECOND * 2);
+  });
+})();
+
+const cpuCount = new Prometheus.Gauge({
+  name: 'node_cpu_count',
+  help: 'Number of CPUs',
+});
+(function cpuCountReporting() {
+  cpuCount.set(osUtils.countCPUs());
+  setTimeout(cpuCountReporting, ONE_MINUTE * 30);
+})();
+
+const loadAvg = {
+  one: new Prometheus.Gauge({
+    name: 'node_loadavg_1',
+    help: 'Load average (1 minute)',
+  }),
+  five: new Prometheus.Gauge({
+    name: 'node_loadavg_5',
+    help: 'Load average (5 minutes)',
+  }),
+  fifteen: new Prometheus.Gauge({
+    name: 'node_loadavg_15',
+    help: 'Load average (15 minutes)',
+  }),
+};
+(function loadAvgReporting() {
+  loadAvg.one = osUtils.loadavg(1);
+  loadAvg.five = osUtils.loadavg(5);
+  loadAvg.fifteen = osUtils.loadavg(15);
+  setTimeout(loadAvgReporting, ONE_MINUTE);
+})();
 
 module.exports = expressPromBundle({
-  // includeMethod: true,
-  // includeStatusCode: true,
-  // includePath: true,
+  includeMethod: true,
+  includeStatusCode: true,
+  includePath: true,
 });
