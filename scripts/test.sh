add2075c138d0a037881741cfb796c98ae659ba2
zephinzer <dev@joeir.net>
2018-01-15 22:33:55 +0800

    added lcov reporter for istanbul

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,10 @@ printf "script directory         : ${CURR_DIR}\n";
 printf "using mocha              : ${MOCHA_VERSION}\n";
 printf "test run started on      : $(date +'%d-%m-%Y %H:%M:%S')\n";
 
-istanbul cover ./node_modules/.bin/_mocha -- \
-  --reporter dot \
-  --forbid-only \
-  --forbid-pending "./test/entrypoint.js" "./test/**/*.test.js";
+istanbul cover --report lcov \
+  ./node_modules/.bin/_mocha -- \
+    --reporter dot \
+    --forbid-only \
+    --forbid-pending \
+    "./test/entrypoint.js" \
+    "./test/**/*.test.js";
